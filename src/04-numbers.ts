e9d84f3af5390345cb1a7cf79cfaf3765e1b9afb
Diego <dsantacruz@gmail.com>
2022-04-24 00:12:58 -0300

    Numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,34 @@
+(() => {
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('productPrice', productPrice);
+
+  let customerAge: number = 28;
+  //customerAge = customerAge + '1' ;
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge);
+
+  let producInStock: number;
+  console.log('producInStock', producInStock);
+
+  if (producInStock > 10) {
+    console.log('is greater');
+  }
+
+  let discont = parseInt('100');
+  console.log('discont', discont);
+
+  if (discont <= 200) {
+    console.log('apply');
+  } else {
+    console.log('not apply');
+  }
+
+  let hex = 0xfff;
+  console.log('hex', hex);
+  let bin = 0b01010;
+  console.log('bin',bin);
+
+  const myNumber: number = 10;
+
+})();
