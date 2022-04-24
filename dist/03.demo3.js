520793e725811e4edc4966b3541bb138c7c5ded8
Diego <dsantacruz@gmail.com>
2022-04-23 22:58:14 -0300

    Demo 3

---
+++ dist/03.demo3.js
@@ -0,0 +1,11 @@
+"use strict";
+//const productPrice: number = 12;
+(() => {
+    let myProductName = 'Product 1';
+    let myProductPrice = 123.8789798;
+    myProductName = 'asdas';
+    console.log(myProductName.toLocaleUpperCase());
+    console.log(myProductPrice.toFixed());
+    const myProductStock = 1000;
+    const myProductName2 = 'Product 1';
+})();
